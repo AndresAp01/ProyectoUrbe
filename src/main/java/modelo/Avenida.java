2d88e0c5c16b906a068f5fedccee9145b6ae78bb
SFuness <sfunnes06@gmail.com>
2025-10-05 12:54:54 -0600

    Se agrega Javadoc a Avenida.java

--- src/main/java/modelo/Avenida.java
+++ src/main/java/modelo/Avenida.java
@@ -1,5 +1,9 @@
 package modelo;
 
+/**
+ * Enum que representa las avenidas disponibles en la ciudad.
+ * Se utiliza para ubicar anomalías o referencias de ubicación en el sistema.
+ */
 public enum Avenida {
     Insurgentes, Libertad, LasAméricas, Universidad, Central, Hidalgo, DelParque, Independencia, Principal
 }
