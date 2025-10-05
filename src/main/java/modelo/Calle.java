2eca40a2548ecedf0bb2c1beaf30d626184e089b
SFuness <sfunnes06@gmail.com>
2025-10-05 12:55:55 -0600

    Se agrega Javadoc a Calle.java

--- src/main/java/modelo/Calle.java
+++ src/main/java/modelo/Calle.java
@@ -1,5 +1,8 @@
 package modelo;
 
-public enum Calle {
-    Primavera, LosPinos, Luna, LasFlores, DelSol, Olivo, SanMartín, RíoVerde, Esperanza, LaPaz
+/**
+ * Enum que representa las calles disponibles en la ciudad.
+ * Se utiliza para ubicar anomalías o referencias de ubicación en el sistema.
+ */
+public enum Calle {Primavera, LosPinos, Luna, LasFlores, DelSol, Olivo, SanMartín, RíoVerde, Esperanza, LaPaz
 }
